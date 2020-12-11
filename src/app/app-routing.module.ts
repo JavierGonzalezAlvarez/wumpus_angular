64fdaf401d34dbc1341f0597102b87144ea2d758
JavierGonzalezAlvarez <jgalvarez.jga@gmail.com>
2020-12-11 21:30:12 +0100

    favicon_rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BodyComponent} from "./components/body/body.component";
+import { HelpComponent } from "./components/help/help.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // rutas en angular
+    path: '', component: BodyComponent
+  },
+  {
+    // rutas en angular
+    path: 'help', component: HelpComponent
+  },  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
